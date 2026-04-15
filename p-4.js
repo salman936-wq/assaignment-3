74b2bad42118cea8517765d86a3bb13b356c66ac
TANVIR AHMAD SALMAN <salmansardar936@gmail.com>
2026-04-15 07:01:06 +0600

    Refactor gonoVote function for clarity and accuracy

--- p-4.js
+++ p-4.js
@@ -1,32 +1,35 @@
 function gonoVote(array) {
-     let ha=0;
-     let na=0;
 
-     if(!Array.isArray(array)){
-        return 'Invalid';
-       }
-      for(let arra of array){
-         if(arra==='ha'){
-          ha++;
-         }
-         else if(arra==="na"){
-            na++;
-         }
-       
-      }
+    if (!Array.isArray(array)) {
+        return "Invalid";
+    }
 
-      if(ha>na){
-        return 'true';
-      }
-      else if(ha<na){
-        return 'false';
-      }
-      else if(ha===na){
-        return 'equal';
-      }
+    let haCount = 0;
+    let naCount = 0;
 
-
+    for (let vote of array) {
+        if (typeof vote !== "string") {
+            return "Invalid";
+        }
+        if (vote === "ha") {
+            haCount++
+        }
+        else if (vote === "na") {
+            naCount++
+        }
+    }
+    
+    if (haCount > naCount) {
+        return true;
+    }
+    else if (naCount > haCount) {
+        return false;
+    }
+    else if (haCount === naCount) {
+        return "equal";
+    }
 }
 
-
-
+// const votes = ["ha, na"];
+// const result = gonoVote(votes);
+// console.log(result);
